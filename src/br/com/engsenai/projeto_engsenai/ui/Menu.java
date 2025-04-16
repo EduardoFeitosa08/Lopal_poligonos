a39aaa3879da2b42b3f4d9208ddcbdc92175f3dc
EDUARDO FEITOSA BATISTA <25132675@CFP127ALUNO.LOCAL>
2025-04-16 16:59:24 -0300

    Iniciado as opções para o usuario escolher

--- src/br/com/engsenai/projeto_engsenai/ui/Menu.java
+++ src/br/com/engsenai/projeto_engsenai/ui/Menu.java
@@ -2,9 +2,18 @@ package br.com.engsenai.projeto_engsenai.ui;
 
 import java.util.Scanner;
 
+import br.com.engsenai.projeto_engsenai.model.Circuferencia;
+import br.com.engsenai.projeto_engsenai.model.Quadrado;
+import br.com.engsenai.projeto_engsenai.model.Retangulo;
+import br.com.engsenai.projeto_engsenai.model.Trapezio;
+import br.com.engsenai.projeto_engsenai.model.Triangulo;
+
 public class Menu {
 
 	public void criarMenu() {
+		
+		Scanner leitor = new Scanner(System.in);
+		
 		System.out.println("---------------------------------");
 		System.out.println("CALCULADORA DE POLIGONOS");
 		System.out.println("---------------------------------");
@@ -17,10 +26,69 @@ public class Menu {
 		System.out.println("---------------------------------");
 		System.out.print("Escolha uma opção (1 - 6):");
 		
-		Scanner leitor = new Scanner(System.in);
-		int opcao = leitor.nextInt();
-		System.out.println("Você pensou na opção " + opcao);
 		
+		int opcao = leitor.nextInt();
+		
+		if (opcao == 1) {
+			
+			Quadrado q1 = new Quadrado();
+			
+			System.out.print("Defina o lado: ");
+			double lado = leitor.nextDouble();
+			q1.setLado(lado);
+			q1.mostrarDados();
+		}
+		
+		if (opcao == 2) {
+			
+			Retangulo r1 = new Retangulo();
+			
+			System.out.print("Defina a base: ");
+			double base = leitor.nextDouble();
+			r1.setBase(base);
+			System.out.print("Defina a altura: ");
+			double altura = leitor.nextDouble();
+			r1.setAltura(altura);
+			r1.mostrarDados();
+		}
+		
+		if (opcao == 3) {
+			
+			Trapezio tr1 = new Trapezio();
+			
+			System.out.print("Defina a base menor: ");
+			double baseMenor = leitor.nextDouble();
+			tr1.setBaseMenor(baseMenor);
+			System.out.print("Defina a base maior: ");
+			double baseMaior = leitor.nextDouble();
+			tr1.setBaseMaior(baseMaior);
+			System.out.print("Defina a altura: ");
+			double altura = leitor.nextDouble();
+			tr1.setAltura(altura);
+			tr1.mostrarDados();
+		}
+		
+		if (opcao == 4) {
+			
+			Triangulo t1 = new Triangulo();
+			
+			System.out.print("Defina a base: ");
+			double base = leitor.nextDouble();
+			t1.setBase(base);
+			System.out.print("Defina a altura: ");
+			double altura = leitor.nextDouble();
+			t1.setAltura(altura);
+			t1.mostrarDados();
+		}
+		
+		if (opcao == 5) {
+			
+			Circuferencia c1 = new Circuferencia();
+			
+			System.out.print("Defina o raio: ");
+			double raio = leitor.nextDouble();
+			c1.setRaio(raio);
+			c1.mostrarDados();
+		}
 	}
-	
 }
